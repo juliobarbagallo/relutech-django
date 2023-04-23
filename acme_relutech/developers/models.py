c8a9a1141f75ff39245e6a82af7c9d7f754d0068
juliobarbagallo <julio.barbagallo@gmail.com>
2023-04-23 16:30:02 -0300

    feat: Changes name from users to developers and add tokens

--- acme_relutech/developers/models.py
+++ acme_relutech/developers/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-class User(models.Model):
+class Developer(models.Model):
     fullname = models.CharField(max_length=255)
     active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.fullname
