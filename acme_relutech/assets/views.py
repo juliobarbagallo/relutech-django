53c8e7ff6668cd592b5617300036bdf7de99cb11
juliobarbagallo <julio.barbagallo@gmail.com>
2023-04-23 15:31:46 -0300

    feat: Adds models for users and assets and assets crud ops

--- acme_relutech/assets/views.py
+++ acme_relutech/assets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Asset
+from .serializers import AssetSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class AssetViewSet(viewsets.ModelViewSet):
+    queryset = Asset.objects.all()
+    serializer_class = AssetSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
