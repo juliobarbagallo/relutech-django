c6b8552d0672b0eeaa26ea0a45fb2949fdf62e87
juliobarbagallo <julio.barbagallo@gmail.com>
2023-04-23 15:35:26 -0300

    fix: import status

--- acme_relutech/assets/views.py
+++ acme_relutech/assets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Asset
 from .serializers import AssetSerializer
 from rest_framework.response import Response
